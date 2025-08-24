ac4428090681f631a8b7fb0e9c16fba218788774
Kaeleb Imlay <kaeimlay@gmail.com>
2025-08-24 17:21:56 +0000

    Add gas optimization analysis and automated audit documentation generation

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,16 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     hardhat: {}
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    gasPrice: 0.1, // Arbitrum gas price
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
   }
 };
